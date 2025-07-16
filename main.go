7de6aab73d05398b1ee5e2b99bbd75da5668d3d5
pk <pk@codeblob.work>
2025-07-16 23:32:07 +0200

    Bump version

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	app := &cli.App{
 		Name:    "gowebbuild",
 		Usage:   "All in one tool to build web frontend projects.",
-		Version: "7.2.0",
+		Version: "7.3.0",
 		Authors: []*cli.Author{{
 			Name: "trading-peter (https://github.com/trading-peter)",
 		}},
